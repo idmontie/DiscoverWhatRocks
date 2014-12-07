39b3048ee5385103ca3794d8a79beadd5298e787
Ivan Montiel <idmontie@asu.edu>
2014-12-07 13:12:29 -0700

    geolocation.

---
+++ client/template-helpers/meetup-form-template-helper.js
@@ -0,0 +1,19 @@
+Template['meetups-add-form'].helpers( {
+  geolocation_enabled : function () {
+    'use strict';
+    
+    if ( navigator.geolocation ) {
+      return '';
+    } else {
+      return 'disabled="disabled"';
+    }
+  }
+} );
+
+Template['meetups-add-form'].events( {
+  'click #use_current_location:not("[disabled]")' : function ( e ) {
+    e.preventDefault();
+
+    navigator.geolocation.getCurrentPosition( current_position_callback );
+  }
+} );
