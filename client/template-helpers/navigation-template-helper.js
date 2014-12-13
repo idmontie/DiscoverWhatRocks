7e326709348e10955fdd508729996043e3e6db83
Ivan Montiel <idmontie@asu.edu>
2014-12-12 23:44:51 -0700

    styling front page.

--- client/template-helpers/navigation-template-helper.js
+++ client/template-helpers/navigation-template-helper.js
@@ -1,11 +1,26 @@
 Template.navigation.helpers( {
-  circles : function ( e ) {
-    return Circles.find();
+  circles : function () {
+    'use strict';
+
+    return Circles.find()
+  },
+  meteor_loggedin : function () {
+    'use strict';
+    return !! Meteor.user()
   }
 } );
 
 Template.navigation.events( {
   'click ' : function ( e ) {
     // TODO close the navigation
+  },
+  'click .logout' : function ( e ) {
+    'use strict';
+
+    e.preventDefault()
+
+    Meteor.logout()
+
+    Router.go( '/' )
   }
 } );
