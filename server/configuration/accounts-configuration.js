2b20f777152653423c7d149637567e46f41a859e
Ivan Montiel <idmontie@asu.edu>
2015-01-22 07:45:33 -0700

    initial

---
+++ server/configuration/accounts-configuration.js
@@ -0,0 +1,42 @@
+AccountsTemplates.configure({
+
+    // Appearance
+    //showAddRemoveServices: false,
+    showForgotPasswordLink: false,
+    showLabels: true,
+    showPlaceholders: true,
+
+    // Client-side Validation
+    continuousValidation: false,
+    negativeFeedback: false,
+    negativeValidation: true,
+    positiveValidation: true,
+    positiveFeedback: true,
+    showValidating: true,
+
+    // Privacy Policy and Terms of Use
+    privacyUrl: 'privacy',
+    termsUrl: 'terms-of-use',
+
+    // Redirects
+    homeRoutePath: '/',
+    redirectTimeout: 4000,
+
+    // Hooks
+    // onLogoutHook: myLogoutFunc,
+    // onSubmitHook: mySubmitFunc,
+
+    // Texts
+    texts: {
+      button: {
+          signUp: "Sign Up Now!"
+      },
+      socialSignUp: "Sign Up",
+      socialIcons: {
+          "meteor-developer": "fa fa-rocket"
+      },
+      title: {
+          forgotPwd: "Recover Your Password"
+      },
+    },
+});
